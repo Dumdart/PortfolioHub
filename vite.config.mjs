390efc4cd865716c5478f0bbb61baf3e88c7ec11
Paul Thumfart <158182803+Dumdart@users.noreply.github.com>
2026-07-27 10:51:50 +0200

    Portfolio: Add Initial Portfolio Experience

---
+++ vite.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  build: {
+    outDir: "dist/client",
+  },
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: ["terminal.local", "localhost"],
+    warmup: {
+      clientFiles: ["./src/main.ts"],
+    },
+  },
+  plugins: [vue()],
+});
